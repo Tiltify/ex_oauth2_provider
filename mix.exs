c15bd7eae12fa407e0b29f09197a9762231ef86b
Adam Malosh <adam@tiltify.com>
2024-08-08 12:05:56 -0700

    update for elixir 17

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExOauth2Provider.Mixfile do
     [
       app: :ex_oauth2_provider,
       version: @version,
-      elixir: "~> 1.14.2",
+      elixir: "~> 1.17.2",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
       deps: deps(),
